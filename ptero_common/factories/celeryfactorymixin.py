7d9124fbb479881ec8d5df13d7fe83c12ed82113
Michael J. Kiwala <mkiwala@genome.wustl.edu>
2015-12-15 16:41:59 +0000

    Make celery initialization abstract

--- ptero_common/factories/celeryfactorymixin.py
+++ ptero_common/factories/celeryfactorymixin.py
@@ -7,7 +7,6 @@ class CeleryFactoryMixin(object):
     def __init__(self, celery_app=None):
         self.celery_app = celery_app
 
+    @abc.abstractmethod
     def _initialize_celery(self):
-        if self.celery_app is None:
-            from ptero_lsf.implementation.celery_app import app
-            self.celery_app = app
+        pass

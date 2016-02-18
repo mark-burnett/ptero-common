7c7724c92bf46ff60d3633e2275045f9505a099f
Michael J. Kiwala <mkiwala@genome.wustl.edu>
2016-02-18 16:55:30 +0000

    Move sends_404 into view_wrapper module

---
+++ ptero_common/view_wrapper.py
@@ -0,0 +1,13 @@
+from functools import wraps
+from ptero_common.exceptions import NoSuchEntityError
+
+
+def sends_404(target):
+    @wraps(target)
+    def wrapper(*args, **kwargs):
+        try:
+            result = target(*args, **kwargs)
+        except NoSuchEntityError as e:
+            return {'error': e.message}, 404
+        return result
+    return wrapper

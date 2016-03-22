a4b3c0c4eb2cb16b142b9c59ede0ea0fc076b960
David Morton <davidlmorton@gmail.com>
2016-03-22 03:39:13 +0000

    Add submitted status

--- ptero_common/statuses.py
+++ ptero_common/statuses.py
@@ -1,5 +1,6 @@
 new = 'new'
 scheduled = 'scheduled'
+submitted = 'submitted'
 running = 'running'
 succeeded = 'succeeded'
 failed = 'failed'
@@ -8,12 +9,13 @@ canceled = 'canceled'
 
 TERMINAL_STATUSES = (succeeded, failed, errored, canceled)
 
-VALID_STATUSES = (new, scheduled, running) + TERMINAL_STATUSES
+VALID_STATUSES = (new, scheduled, submitted, running) + TERMINAL_STATUSES
 
 VALID_STATUS_TRANSITIONS = {
     new: (scheduled, running, failed, errored, succeeded,
         canceled),
-    scheduled: (running, failed, errored, succeeded, canceled),
+    scheduled: (submitted, running, failed, errored, succeeded, canceled),
+    submitted: (running, failed, errored, succeeded, canceled),
     running: (failed, errored, succeeded, canceled),
     failed: tuple(),
     errored: tuple(),
